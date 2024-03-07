139bee00ef5cea5f8f4135072d995d95ccfe8989
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2024-03-07 19:24:50 -0300

    test: adicionando view de teste

--- TheMovie/TheMovie/Scenes/Splash/SplashViewController.swift
+++ TheMovie/TheMovie/Scenes/Splash/SplashViewController.swift
@@ -9,8 +9,34 @@ import UIKit
 
 class SplashViewController: UIViewController {
     
+    lazy var centerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = ColorConstants.backColor
+        setupView()
     }
+    
+    func setupView() {
+        setupViewHierarchy()
+        setupConstraints()
+    }
+    private func setupViewHierarchy() {
+        view.addSubview(centerView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            centerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            centerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            centerView.widthAnchor.constraint(equalToConstant: 300),
+            centerView.heightAnchor.constraint(equalToConstant: 249)
+        ])
+    }
+    
 }
