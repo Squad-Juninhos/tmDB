2fe867025c50eeea6f0a37098e1964a9e664c384
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2024-03-13 15:16:31 -0300

    fix: reutilizando codigo + centralização de animationView

--- TheMovie/TheMovie/Scenes/Splash/SplashViewController.swift
+++ TheMovie/TheMovie/Scenes/Splash/SplashViewController.swift
@@ -3,15 +3,48 @@ import Lottie
 
 class SplashViewController: UIViewController {
     
-    var animationView: LottieAnimationView?
+    lazy var centerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var animationView: LottieAnimationView = {
+        let animationView = LottieAnimationView(name: "tela")
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.loopMode = .loop
+        animationView.play()
+        animationView.contentMode = .scaleAspectFill
+        return animationView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = ColorConstants.backColor
-        animationView = .init(name: "tela")
-        animationView?.frame = view.bounds
-        animationView?.loopMode = .loop
-        view.addSubview(animationView!)
-        animationView?.play()
+        setupView()
+    }
+    
+    func setupView() {
+        setupViewHierarchy()
+        setupConstraints()
+    }
+    
+    func setupViewHierarchy() {
+        centerView.addSubview(animationView)
+        view.addSubview(centerView)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            centerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            centerView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -30),
+            centerView.widthAnchor.constraint(equalToConstant: 300),
+            centerView.heightAnchor.constraint(equalToConstant: 280),
+            
+            animationView.centerXAnchor.constraint(equalTo: centerView.centerXAnchor),
+            animationView.centerYAnchor.constraint(equalTo: centerView.centerYAnchor),
+            animationView.widthAnchor.constraint(equalTo: centerView.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: centerView.heightAnchor)
+        ])
     }
 }
