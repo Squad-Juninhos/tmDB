30407250fafbfa0c741e405bce6447e72baf87c5
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2024-03-15 18:00:18 -0300

    fix: organizando código
    - Remoção de magic numbers;
    - Remoção de Color(era uma pasta);
    - Adicionando novos arquivos p/ criação e estilo do botão;
    -Adicionando imagem no botão;
    -

---
+++ TheMovie/TheMovie/Common/Constants/Text.swift
@@ -0,0 +1,19 @@
+//
+//  Text.swift
+//  TheMovie
+//
+//  Created by Jeiel Lima on 15/03/24.
+//
+
+import Foundation
+
+struct Text {
+    struct Auth {
+        struct Login {
+            static let socialSignIn = "Sign in with social accounts"
+            static let or           = "or"
+            static let btnSignIn    = "Sign In"
+            static let createButton = "Create a Free Account"
+        }
+    }
+}
